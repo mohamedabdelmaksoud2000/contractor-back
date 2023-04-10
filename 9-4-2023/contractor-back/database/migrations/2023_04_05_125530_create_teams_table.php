4575c1a7471be8115afa8b70cd34a25f6477af40
abdoprograming <abdulrahmanahmedabuoreej@gmail.com>
2023-04-10 23:29:38 +0300

    after make Controller recourc and put valdiation in userControler and CompanyController

---
+++ 9-4-2023/contractor-back/database/migrations/2023_04_05_125530_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('describe');
+            $table->unsignedBigInteger('supervisor_id');
+            $table->string('image');
+            $table->timestamps();
+            $table->foreign('supervisor_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
